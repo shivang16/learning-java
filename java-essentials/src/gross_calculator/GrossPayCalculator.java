52401c44f09494e418c81ced4ffea1eb82055c99
Shivang Mangal <shivangmangal@gmail.com>
2022-12-10 14:37:43 +0530

    variables

--- java-essentials/src/gross_calculator/GrossPayCalculator.java
+++ java-essentials/src/gross_calculator/GrossPayCalculator.java
@@ -1,8 +1,20 @@
 package gross_calculator;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of hours worked: ");
+        int hours = scanner.nextInt();
+
+        System.out.println("Enter pay rate: ");
+        double payRate = scanner.nextDouble();
+
+        double totalRate = hours*payRate;
+
+        System.out.println("Total rate="+totalRate);
     }
 }
