a8f5cae12d379bb1bc5de5f249165d7f2ca358f6
Shivang Mangal <shivangmangal@gmail.com>
2023-01-04 17:46:43 +0530

    l2p2 done

--- oracleJavaSE17/src/lesson3/Practice2.java
+++ oracleJavaSE17/src/lesson3/Practice2.java
@@ -1,4 +1,20 @@
 package lesson3;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Practice2 {
+    public static void main(String[] args) {
+        double price = 1.85;
+        double rate = 0.065;
+        price-=price*rate;
+        price = Math.round(price*100)/100.0;
+        System.out.println(price);
+
+        BigDecimal dprice = new BigDecimal(1.85);
+        BigDecimal drate = new BigDecimal(0.065);
+        double numCustomer = 10000;
+        dprice = dprice.subtract(dprice.multiply(drate)).setScale(2, RoundingMode.HALF_UP);
+        System.out.println(dprice);
+    }
 }
